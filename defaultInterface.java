d169524a5170647863e9406abd8a43820a28fb89
ck2577 <160083274+ck2577@users.noreply.github.com>
2024-02-14 21:53:50 -0500

    file added

---
+++ defaultInterface.java
@@ -0,0 +1,30 @@
+package com.chid.utility;
+
+interface defaultinter
+{
+	default void add() {
+		System.out.println("interface add method");
+	}
+	default void subtract()
+	{
+		System.out.println("interfcae subtract method");
+	}
+	static void multiply()
+	{
+		System.out.println("interface multiply method");
+	}
+}
+
+public class defaultInterface implements defaultinter{
+	public static void main(String ar[])
+			{
+				defaultInterface obj = new defaultInterface();
+				obj.add();
+				obj.subtract();
+				defaultinter.multiply();
+						
+				
+			}
+
+	
+}
